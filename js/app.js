7e7633335f85de3fa2e0a6aa44581e6b6a832b1f
Orcun Selbasan <lutfuorcunselbasan@users.noreply.github.com>
2021-10-08 16:26:59 +0300

    Fixed negative ticket number, added form validation

--- js/app.js
+++ js/app.js
@@ -19,82 +19,6 @@ let [from, to, dep, ret, priceSubtotal, priceVat, priceTotal, vipInput, firstInp
   form.querySelector("#economy-ticket"),
 ];
 
-let info = [,,,,,];
-let [vipSeat, firstSeat, ecoSeat] = [0, 0, 0];
-
-vipInput.addEventListener('change', (e) => {
-  vipSeat = parseInt(e.target.value);
-  priceUpdate();
-});
-firstInput.addEventListener('change', (e) => {
-  firstSeat = parseInt(e.target.value);
-  priceUpdate();
-});
-ecoInput.addEventListener('change', (e) => {
-  ecoSeat = parseInt(e.target.value);
-  priceUpdate();
-});
-
-// FORM EVENT LISTENER
-form.addEventListener("submit", (e) => {
-
-  //DESTINATON
-  if (from.value == "From" || to.value == "To") {
-    alert("Invalid destination!");
-  } else if (from.value === to.value) { 
-    alert("Destinations cannot be the same!");
-  }else {
-    // Store destination info
-    info[0]=([from.options[from.selectedIndex].text, to.options[to.selectedIndex].text]);
-  }
-
-  //TIME
-  const depDate = new Date(dep.value);
-  const retDate = new Date(ret.value);
-  if (retDate.getTime() < depDate.getTime()) {
-    alert("Invalid Date");
-  } else {
-    depVal =
-      dep.value /*[depDate.getDate(), depDate.getMonth(), depDate.getFullYear()]*/;
-    retVal =
-      ret.value /*[retDate.getDate(), retDate.getMonth(), retDate.getFullYear()]*/;
-    // Store time info
-    info[1]=([depVal, retVal]);
-  }
-
-  //UPDATE INFO
-
-  // THOSE CAN BE UPDATE UNTIL CLICKING BOOK NOW BUTTON AS THOSE ARE OUTSIDE OF EVENT LISTENER
-  // Store seat amount
-  info[2] = ([vipSeat, firstSeat, ecoSeat]);
-  // Store total price of each ticket
-  info[3] = ([vipSeat * 7000, firstSeat * 1500, ecoSeat * 1000]);
-
-
-  localStorage.setItem("from", info[0][0]);
-  localStorage.setItem("to", info[0][1]);
-  localStorage.setItem("dep", info[1][0]);
-  localStorage.setItem("ret", info[1][1]);
-  localStorage.setItem("vipSeat", info[2][0]);
-  localStorage.setItem("firstSeat", info[2][1]);
-  localStorage.setItem("ecoSeat", info[2][2]);
-  localStorage.setItem("vipPrice", info[3][0]);
-  localStorage.setItem("firstPrice", info[3][1]);
-  localStorage.setItem("ecoPrice", info[3][2]);
-  localStorage.setItem("totalPrice", info[4][0]);
-
-});
-
-// Live price update 
-const priceUpdate = () => {
-  let subtotal = vipSeat*7000 + firstSeat*1500 + ecoSeat*1000;
-  priceSubtotal.innerText = `$${subtotal}`;
-  priceVat.innerText = `$${subtotal / 10}`;
-  priceTotal.innerText = `$${subtotal + subtotal / 10}`;
-  // Store total price of all tickets
-  info[4] = ([subtotal + subtotal / 10]);
-}
-
 // INCREASE TICKET
 const increaseValue = (selector) => {
   var value = parseInt(document.getElementById(selector).value);
@@ -152,3 +76,99 @@ const decreaseValue = (selector) => {
   }
   document.getElementById(selector).value = value;
 };
+
+
+// Live price update 
+const priceUpdate = () => {
+  let subtotal = vipSeat*7000 + firstSeat*1500 + ecoSeat*1000;
+  priceSubtotal.innerText = `$${subtotal}`;
+  priceVat.innerText = `$${subtotal / 10}`;
+  priceTotal.innerText = `$${subtotal + subtotal / 10}`;
+  // Store total price of all tickets
+  info[4] = ([subtotal + subtotal / 10]);
+}
+
+let info = [undefined,undefined,undefined,undefined,undefined];
+let [vipSeat, firstSeat, ecoSeat] = [0, 0, 0];
+
+vipInput.addEventListener('change', (e) => {
+  if (isNaN(parseInt(e.target.value))) e.target.value = 0;
+  vipSeat = parseInt(e.target.value);
+  priceUpdate();
+});
+firstInput.addEventListener('change', (e) => {
+  if (isNaN(parseInt(e.target.value))) e.target.value = 0;
+  firstSeat = parseInt(e.target.value);
+  priceUpdate();
+});
+ecoInput.addEventListener('change', (e) => {
+  if (isNaN(parseInt(e.target.value))) e.target.value = 0;
+  ecoSeat = parseInt(e.target.value);
+  priceUpdate();
+});
+
+// FORM EVENT LISTENER
+form.addEventListener("submit", (e) => {
+  
+  let validationFail = false;
+
+  //DESTINATON
+  if (from.value == "From" || to.value == "To") {
+    alert("Invalid destination!");
+    validationFail = true;
+  } else if (from.value === to.value) { 
+    alert("Destinations cannot be the same!");
+    validationFail = true;
+  }else {
+    // Store destination info
+    info[0]=([from.options[from.selectedIndex].text, to.options[to.selectedIndex].text]);
+  }
+
+  //TIME
+  const depDate = new Date(dep.value);
+  const retDate = new Date(ret.value);
+  if (retDate.getTime() < depDate.getTime()) {
+    alert("Invalid Date");
+    validationFail = true;
+  } else {
+    depVal =
+      dep.value /*[depDate.getDate(), depDate.getMonth(), depDate.getFullYear()]*/;
+    retVal =
+      ret.value /*[retDate.getDate(), retDate.getMonth(), retDate.getFullYear()]*/;
+    // Store time info
+    info[1]=([depVal, retVal]);
+  }
+
+  //UPDATE INFO
+
+  // THOSE CAN BE UPDATE UNTIL CLICKING BOOK NOW BUTTON AS THOSE ARE OUTSIDE OF EVENT LISTENER
+  // Store seat amount
+  info[2] = ([vipSeat, firstSeat, ecoSeat]);
+  // Store total price of each ticket
+  info[3] = ([vipSeat * 7000, firstSeat * 1500, ecoSeat * 1000]);
+
+  if (info[2].reduce((a,b) => a+ b) == 0){
+    alert("No tickets added");
+    validationFail = true;
+  }
+  
+  if (validationFail) {
+    e.preventDefault();
+    validationFail = false;
+  }
+
+  localStorage.setItem("from", info[0][0]);
+  localStorage.setItem("to", info[0][1]);
+  localStorage.setItem("dep", info[1][0]);
+  localStorage.setItem("ret", info[1][1]);
+  localStorage.setItem("vipSeat", info[2][0]);
+  localStorage.setItem("firstSeat", info[2][1]);
+  localStorage.setItem("ecoSeat", info[2][2]);
+  localStorage.setItem("vipPrice", info[3][0]);
+  localStorage.setItem("firstPrice", info[3][1]);
+  localStorage.setItem("ecoPrice", info[3][2]);
+  localStorage.setItem("totalPrice", info[4][0]);
+
+});
+
+
